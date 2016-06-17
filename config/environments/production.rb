abc42e4d5f4de8bb047dbdbe2f54ee557e52d19c
Gabriel Viana <gabrielv14@gmail.com>
2016-06-17 18:17:37 -0300

    CSS's and gemfile adjusts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,23 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  Rails.application.routes.default_url_options[:host] = 'localhost'
+   config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    # Configurações de email para o gmail
+    :address        => 'smtp.gmail.com',
+    :domain         => 'taviajando.com',
+    :port           => 587,
+    :user_name      => 'ta.viajando2016@gmail.com',
+    :password       => 'taviajandoproject16',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
 end
