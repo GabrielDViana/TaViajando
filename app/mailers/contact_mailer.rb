a5ca1471f6bb1f248d1ced2bd11e8ae2d109bee0
Gabriel Viana <gabrielv14@gmail.com>
2016-06-17 17:45:23 -0300

    several adjustments to layout

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,12 @@
 class ContactMailer < ActionMailer::Base
 
-  default to: 'gabrielv14@gmail.com'
+  default to: 'ta.viajando2016@gmail.com'
 
-    def contact_email(name, email, body)
+    def contact_email(name, email, phone, body, state)
         @name = name
         @email = email
-        @body = body
         @phone = phone
+        @body = body
         @state = state
         mail(from: 'ta.viajando2016@gmail.com', subject: 'Contato: "Ta Viajando?"')
     end
